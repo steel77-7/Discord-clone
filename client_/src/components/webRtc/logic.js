f5fbb74b406d0b548796d3314e9fa02fed9a2a29
steel77-7 <utkarshkaundal1970@gmail.com>
2024-08-06 03:34:18 +0530

    defining the calling routes

--- client_/src/components/webRtc/logic.js
+++ client_/src/components/webRtc/logic.js
@@ -11,12 +11,9 @@ export async function playVideoFromCamera() {
           max: 768,
         },
       },
-      audio: true,
+      audio: false,
     };
-    /* const constraints = {
-            'video': true,
-            'audio': true
-        } */
+   
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     const videoElement = document.querySelector("video#localVideo");
     videoElement.srcObject = stream;
@@ -25,10 +22,72 @@ export async function playVideoFromCamera() {
   }
 }
 
-const connect = async () => {
-  const peerConnection = new RTCPeerConnection();
-  const remoteStream = new MediaStream();
+function createpeer() {
+  const configuration = {
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  };
+  const peerConnection = new RTCPeerConnection(configuration);
+  let dataChannel = peerConnection.createDataChannel("video-feed");
+  peerConnection.onicecandidate = (e) => {
+    if (e.candidate !== null) {
+      sendCandidateToRemotePeer(e.candidate);
+    }
+  };
+  return { peerConnection, dataChannel };
+}
+
+export const local = async () => {
+  const { peerConnection, dataChannel } = createpeer();
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
+  //data channel handling
+  dataChannel.onopen = () => console.log("Data Channel openned");
+  dataChannel.onclose = () => console.log("Data Channel closed");
+  dataChannel.onmessage = (e) => {
+    //video stream will be handled here
+  };
+  //data channel handling
 };
+
+export async function remote() {
+  const { peerConnection, dataChannel } = createpeer();
+  const remoteConnection = peerConnection;
+  //data channel handling
+  remoteConnection.ondatachannel((e) => {
+    dataChannel = e.channel;
+    dataChannel.onopen = () => console.log("Data Channel openned");
+    dataChannel.onclose = () => console.log("Data Channel closed");
+    dataChannel.onmessage = (e) => {
+      //video stream will be handled here
+    };
+    //data channel handling
+    const stream = getVideoFeed();
+    if (stream !== null) {
+      dataChannel.send(stream);
+    }
+  });
+}
+
+
+//get video feed
+async function getVideoFeed() {
+  const constraints = {
+    video: {
+      width: {
+        min: 640,
+        max: 1024,
+      },
+      height: {
+        min: 480,
+        max: 768,
+      },
+    },
+    audio: false,
+  };
+
+  //gathering media stream
+  let stream = null;
+  stream = await navigator.mediaDevices.getUserMedia(constraints);
+  return stream;
+}
--- server/socketServer.js
+++ server/socketServer.js
@@ -1,38 +1,59 @@
-
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 
 const socketConnection = (httpServer) => {
-  console.log('Socket server initialized');
+  console.log("Socket server initialized");
   const io = new Server(httpServer, {
     cors: {
       origin: "*",
-      methods: ["GET", "POST"]
-    }
+      methods: ["GET", "POST"],
+    },
   });
-  
 
-  io.on('connection', (socket) => {
-    console.log('Socket connected', socket.id);
+  io.on("connection", (socket) => {
+    console.log("Socket connected", socket.id);
 
-    socket.on('join-room', (room) => {
+    socket.on("join-room", (room) => {
       socket.join(room);
       console.log(`Joined room: ${room}`);
     });
 
-    socket.on('send-message', (message) => {
-      console.log('Received direct message:', message);
-      socket.to(message.chat._id).emit('recieve-message' ,(message));
+    socket.on("send-message", (message) => {
+      console.log("Received direct message:", message);
+      socket.to(message.chat._id).emit("recieve-message", message);
+    });
+    //signalling webrtc
+    socket.on("call-request", (room) => {
+      console.log("Joining the call room");
+      socket.join(room);
     });
 
-    socket.on('disconnect', (reason) => {
+    //signaling ice candidates
+    socket.on("ice-candidate", (data) => {
+      console.log("ice-candidate", data);
+      socket.to(data.room).emit("ice-candidate", data.candidate);
+    });
+
+    //signalling sdp and the members
+    socket.on("offer", (data) => {
+      console.log("user details and the offer", data);
+      socket.to(data.room).emit("offer", data);
+    });
+
+    //signaling answer to all the members in the chat
+    socket.on("answer", (data) => {
+      console.log("user details and the answer", data);
+      socket.to(data.room).emit("asnwer", data);
+    });
+    //signalling rtc
+
+    socket.on("disconnect", (reason) => {
       console.log(`Socket disconnected: ${reason}`);
     });
 
-    socket.on('connect_error', (err) => {
+    socket.on("connect_error", (err) => {
       console.log(`Connection error: ${err.message}`);
     });
   });
 };
 
 module.exports = socketConnection;
-
